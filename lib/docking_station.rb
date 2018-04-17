f9e7e216b9d6bd8e392ce9752eb858e3b5fa9285
Charlene <charlenechetcuti@gmail.com>
2018-04-17 15:56:11 +0100

    added bike working test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
+require './lib/bike'
+
 class DockingStation
   def release_bike
-    
+    bike = Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,16 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   docking_station = DockingStation.new
+  bike = Bike.new
   it "should respond" do
     expect(docking_station).to respond_to(:release_bike)
+  end
+  it "should get a bike when asked to release_bike" do
+    expect(docking_station.release_bike).to be_instance_of(Bike)
+  end
+  it "should expect the bike to be working" do
+    expect(bike).to be_working
   end
 end
