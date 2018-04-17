a3bf646f8b95a463851d585ad22f2954e90f72f1
Muzzi Aldean <amiraldean@gmail.com>
2018-04-17 15:01:55 +0100

    Created release method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  docking_station = DockingStation.new
+  it "should respond" do
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
