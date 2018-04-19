74e7314a4fc72df391f0002fab2d5b043802a792
Mark Channer <markchanner@gmail.com>
2018-04-19 14:21:55 +0100

    Allows working variable to bet set

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,12 @@
 class Bike
-  def working?
-    true
+
+  attr_reader :working
+
+  def initialize(working = true)
+    @working = working
+  end
+
+  def working=(working)
+    @working = working
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,14 @@
 require 'bike'
 
 describe Bike do
-  bike = Bike.new
 
-  it {is_expected.to be_working}
+  it "Checks working is initialzed to true" do
+    expect(subject.working).to eq true    
+  end
+
+  it "Set to be not working" do
+    subject.working = false
+    expect(subject.working).to eq false
+  end
 
 end
