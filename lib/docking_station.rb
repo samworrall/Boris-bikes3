ba87031951e292b380d44276131ad2bb29735f36
Mark Channer <markchanner@gmail.com>
2018-04-19 11:45:53 +0100

    Adds cap on bike capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 require './lib/bike'
 
 class DockingStation
+  CAPACITY = 20
   attr_reader :bikes
 
   def initialize
@@ -8,12 +9,12 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes docked at this station.' if @bikes == []
+    fail 'No bikes docked at this station.' if @bikes.empty?
     @bikes
   end
 
   def dock_bike(bike)
-    fail 'Docking station is at full capacity.' unless @bikes == []
+    fail 'Docking station is at full capacity.' if @bikes.count >= 20
     @bikes.push(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,28 +6,33 @@ describe DockingStation do
   it {is_expected.to respond_to(:release_bike).with(0).argument}
 
   describe '#release_bike' do
-    docking_station = DockingStation.new
+
     it "raises an error when there are no bikes docked" do
-      expect{docking_station.release_bike}.to raise_error 'No bikes docked at this station.'
+      expect{subject.release_bike}.to raise_error 'No bikes docked at this station.'
     end
     it "should get a bike when asked to release a bike" do
       bike = Bike.new
-      docking_station.dock_bike(bike)
-      expect(docking_station.release_bike.last).to be_instance_of(Bike)
+      subject.dock_bike(bike)
+      expect(subject.release_bike.last).to be_instance_of(Bike)
     end
   end
 
   it {is_expected.to respond_to(:dock_bike).with(1).argument}
 
   describe '#dock_bike' do
-    docking_station = DockingStation.new
+
     it "should allow bike to be docked" do
-      docking_station.dock_bike(bike = Bike.new)
-      expect(docking_station.bikes.last).to eq(bike)
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bikes.last).to eq(bike)
     end
+
     it "raises an error when dock station capacity is full" do
-      expect{docking_station.dock_bike(Bike.new)}.to raise_error 'Docking station is at full capacity.'
+      CAPACITY = 20
+      CAPACITY.times { subject.dock_bike(Bike.new) }
+      expect{subject.dock_bike(Bike.new)}.to raise_error 'Docking station is at full capacity.'
     end
+
   end
 
 end
