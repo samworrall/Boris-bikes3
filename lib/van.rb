5fe8b0b0e111c9d0e50331f05d6d5b7b070ebbe3
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-20 14:56:06 +0100

    Adding doubles to van_spec

--- lib/van.rb
+++ lib/van.rb
@@ -8,6 +8,7 @@ class Van
   def collect_broken_bikes(station)
     fail "There are no broken bikes to collect" if station.broken_bikes.empty?
     @bikes + station.broken_bikes
-    station.broken_bikes = []
+    station.broken_bikes.clear
+    @bikes
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,23 +3,20 @@ require 'docking_station'
 require 'bike'
 
 describe Van, :van do
-
+let(:broken_bike) {double :broken_bike, working: false}
+let(:station) {instance_double DockingStation, dock_bike: [broken_bike], broken_bikes: [broken_bike]}
   it 'responds to the method collect broken bikes' do
     expect(subject).to respond_to(:collect_broken_bikes)
   end
 
   describe '#collect_broken_bikes' do
     it 'returns array of broken bikes' do
-      station = DockingStation.new
-      bike = Bike.new(working = false)
-      station.dock_bike(bike)
+      station.dock_bike(broken_bike)
       expect(subject.collect_broken_bikes(station)).to eq station.broken_bikes
     end
 
     it 'raises an error when there are no broken bikes to collect' do
-      station = DockingStation.new
-      bike = Bike.new(working = false)
-      station.dock_bike(bike)
+      station.dock_bike(broken_bike)
       subject.collect_broken_bikes(station)
       expect{subject.collect_broken_bikes(station)}.to raise_error "There are no broken bikes to collect"
     end
