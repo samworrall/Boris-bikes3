c74b99d75d8858f55920743dc386771751ece79c
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-20 09:40:36 +0100

    Adding accessor to bike.rb

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,12 +1,9 @@
 class Bike
 
-  attr_reader :working
+  attr_accessor :working
 
   def initialize(working = true)
     @working = working
   end
-
-  def working=(working)
-    @working = working
-  end
+  
 end
